241b127c4fc356435fa121b766254c00ab959701
bule-ttt <taiseisugawara4@gmail.com>
2026-02-19 06:37:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-      fetch('header.html')
+      fetch('https://blue-ttt.github.io/ooya-pc/header.html')
           .then(response => response.text())
           .then(data => {
                document.getElementById('header').innerHTML = data;
